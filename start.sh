7a6c84964f0b14ec1b50fddcc26ab5a7d8a3e997
Neerj Kumar <119673950+golubhaii@users.noreply.github.com>
2023-09-30 11:58:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/golubhaii/BabyDoll01 /Shortner-Converter-Bot-V2
-cd /Shortner-Converter-Bot-V2
+git clone https://github.com/golubhaii/BabyDoll01 /BabyDoll01
+cd /BabyDoll01
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
